cb046c2987ee9e3db96011ad530e00682b3ae423
Randy <randypriv@gmail.com>
2024-08-03 02:39:31 +0800

    add fav icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://dengtab.com",
   integrations: [react(), tailwind({
     applyBaseStyles: false
-  }), mdx()]
+  }), mdx(), sitemap()]
 });
